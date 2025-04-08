46a21400c868ac81fd7cb38d9b9d41f429960b54
Sehyun Hwang <hwanghyun3@gmail.com>
2025-04-08 15:03:02 +0900

    Docker build and push using cdktf

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,39 +1,41 @@
-import { AwsTerraformAdapter } from "@cdktf/aws-cdk";
-import { DataAwsEcrAuthorizationToken } from "@cdktf/aws-cdk/lib/aws/data-aws-ecr-authorization-token/index.js";
-import { AwsProvider } from "@cdktf/aws-cdk/lib/aws/provider/index.js";
-import { findMapping, registerMapping } from "@cdktf/aws-cdk/lib/mapping/index.js";
-import { EcrRepository } from "@cdktf/provider-aws/lib/ecr-repository/index.js";
-import { IamAccessKey } from "@cdktf/provider-aws/lib/iam-access-key/index.js";
+/* eslint-disable no-new, max-classes-per-file */
+import { join, resolve } from 'path';
+
+import { AwsTerraformAdapter } from '@cdktf/aws-cdk';
+import { DataAwsEcrAuthorizationToken } from '@cdktf/aws-cdk/lib/aws/data-aws-ecr-authorization-token/index.js';
+import { AwsProvider } from '@cdktf/aws-cdk/lib/aws/provider/index.js';
+import { findMapping, registerMapping } from '@cdktf/aws-cdk/lib/mapping/index.js';
+import { EcrRepository } from '@cdktf/provider-aws/lib/ecr-repository/index.js';
+import { IamAccessKey } from '@cdktf/provider-aws/lib/iam-access-key/index.js';
 import { DataLocalFile } from '@cdktf/provider-local/lib/data-local-file/index.js';
 import { File } from '@cdktf/provider-local/lib/file/index.js';
 import { LocalProvider } from '@cdktf/provider-local/lib/provider/index.js';
-import type { ImageArgs } from '@pulumi/docker-build';
-import { Repository } from "aws-cdk-lib/aws-ecr";
+import { Platform, type ImageArgs } from '@pulumi/docker-build';
+import { NODE_REGION_CONFIG_OPTIONS } from '@smithy/config-resolver';
+import { loadConfig } from '@smithy/node-config-provider';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { User } from 'aws-cdk-lib/aws-iam';
-import * as cdktf from "cdktf";
-import { Construct } from "constructs";
-import { join, resolve } from 'path';
-
-import { NODE_REGION_CONFIG_OPTIONS } from "@smithy/config-resolver";
-import { loadConfig } from "@smithy/node-config-provider";
-import { DatabaseInstance } from "aws-cdk-lib/aws-rds";
+import { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
+import * as cdktf from 'cdktf/lib/index.js';
 import { invokeAspects } from 'cdktf/lib/synthesize/synthesizer.js';
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib/core';
 
 const region = await loadConfig(NODE_REGION_CONFIG_OPTIONS)();
-console.log(region);
+console.log({ region });
 
 {
-  const resourceType = "AWS::IAM::User";
+  const resourceType = 'AWS::IAM::User';
   const { resource, attributes } = findMapping(resourceType);
-  registerMapping("AWS::IAM::User", {
+  registerMapping('AWS::IAM::User', {
     resource,
-    attributes(attribute, resource) {
+    attributes(attribute, resource2) {
       if (!(attributes instanceof Function))
         throw new Error();
-      const mapping = attributes(attribute === 'Ref' ? 'UserName' : attribute, resource);
+      const mapping = attributes(attribute === 'Ref' ? 'UserName' : attribute, resource2);
       // console.log({ attribute, resource, mapping });
       return mapping;
-    }
+    },
   });
 }
 
@@ -41,7 +43,7 @@ console.log(region);
   const originalWarn = console.warn;
   const warnedMessages = new Set();
 
-  console.warn = function (...args) {
+  console.warn = function warn(...args) {
     const [message] = args;
     if (!warnedMessages.has(message)) {
       warnedMessages.add(message);
@@ -50,19 +52,22 @@ console.log(region);
   };
 }
 
-export enum ImageNames {
+enum ImageNames {
   KubeApiServerProxy = 'kube-apiserver-proxy',
   IamPgBouncer = 'iam-pgbouncer',
 }
 
 interface BakeTarget extends Omit<ImageArgs, 'context' | 'push'> {
   context: string;
+  platforms: [Platform] | [Platform, Platform] | [Platform, Platform, Platform];
   contexts?: Record<string, string>;
 }
 
 class BuildxBake extends Construct {
   group = {
     default: { targets: [] as string[] },
+    arm64: { targets: [] as string[] },
+    amd64: { targets: [] as string[] },
   };
 
   target: Record<string, BakeTarget> = {};
@@ -75,12 +80,23 @@ class BuildxBake extends Construct {
     const repository = new EcrRepository(scope, 'Repository', {
       name: stack.node.addr.slice(0, 6) + '/' + name,
     });
-    if (args.tags instanceof Promise)
-      throw new Error();
-    const tags = args.tags || [];
-    tags.push(cdktf.Fn.replace(repository.repositoryUrl, '.com', '.com:443'));
-    this.group.default.targets.push(name);
-    this.target[name] = { ...args, tags };
+
+    args.platforms.forEach(platform => {
+      const arch = platform.split('/')[1];
+      const tags = [...(args.tags as string[] || [])];
+      tags.push(repository.repositoryUrl + ':' + arch);
+
+      const targetName = name + '-' + arch;
+      if (['arm64', 'amd64'].includes(arch))
+        this.group[arch as 'arm64' | 'amd64'].targets.push(targetName);
+      this.group.default.targets.push(targetName);
+
+      this.target[targetName] = {
+        ...args,
+        tags,
+        platforms: [platform],
+      };
+    });
   }
 
   generateBakeFile() {
@@ -90,7 +106,7 @@ class BuildxBake extends Construct {
       target: Object.fromEntries(Object.entries(target).map(([key, value]) => [
         key.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`),
         value,
-      ]))
+      ])),
     }, null, 2);
   }
 }
@@ -101,17 +117,22 @@ class ImagesStack extends cdktf.TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
     // new LocalExecProvider(this, "LocalExecProvider");
-    new LocalProvider(this, "LocalProvider");
-    new AwsProvider(this, "Aws", { region });
+    new LocalProvider(this, 'LocalProvider');
+    new AwsProvider(this, 'Aws', { region });
 
+    const { authorizationToken } = new DataAwsEcrAuthorizationToken(this, 'DataAwsEcrAuthorizationToken');
     const buildxBake = new BuildxBake(this, 'BuildxBake');
     buildxBake.addTarget('kube-apiserver-proxy', {
       context: 'kube-apiserver-proxy',
-      platforms: ['linux/arm64', 'linux/amd64'],
+      platforms: [Platform.Linux_arm64, Platform.Linux_amd64],
     });
     buildxBake.addTarget('iam-pgbouncer', {
       context: 'pgbouncer',
-      platforms: ['linux/arm64', 'linux/amd64'],
+      platforms: [Platform.Linux_arm64, Platform.Linux_amd64],
+    });
+    buildxBake.addTarget('coreos-k3s', {
+      context: 'coreos-k3s',
+      platforms: [Platform.Linux_arm64,],
     });
     const bakeFileContent = buildxBake.generateBakeFile();
     console.log('docker-bake.json', bakeFileContent);
@@ -121,30 +142,53 @@ class ImagesStack extends cdktf.TerraformStack {
       content: bakeFileContent,
     });
 
-    const resolveSynthPath = (filename: string) => resolve(join(app.outdir, 'stacks', cdktf.TerraformStack.of(this).node.id, filename));
+    const resolveSynthPath = (filename: string) => resolve(
+      join(cdktf.App.of(this).outdir, 'stacks', cdktf.TerraformStack.of(this).node.id, filename),
+    );
     const metadataFilePath = resolveSynthPath('metadata.json');
     this.metadataFilePath = metadataFilePath;
+
+    const dockerLoginExec = new cdktf.DataResource(this, 'DockerLoginExec', {
+      provisioners: [{
+        type: 'local-exec',
+        command: 'node ' + resolve('docker-login.js'),
+        environment: {
+          AWS_REGION: region,
+          AUTHORIZATION_TOKEN: authorizationToken,
+        },
+      }],
+      triggersReplace: {
+        random: Math.random().toString(),
+      },
+    });
 
     new cdktf.DataResource(this, 'BuildxBakeExec', {
       provisioners: [{
         type: 'local-exec',
-        workingDir: resolve("../"),
+        workingDir: resolve('../'),
         command: `docker-buildx bake --push -f ${resolveSynthPath(bakeFile.filename)} --metadata-file ${metadataFilePath}`,
       }],
-      dependsOn: [bakeFile],
+      dependsOn: [bakeFile, dockerLoginExec],
       triggersReplace: {
-        random: Math.random().toString()
-      }
+        random: Math.random().toString(),
+      },
     });
+
+    // @TODO
+    /* docker-buildx imagetools create
+    248837585826.dkr.ecr.ap-northeast-1.amazonaws.com/c8d557/kube-apiserver-proxy:arm64@sha256:
+    248837585826.dkr.ecr.ap-northeast-1.amazonaws.com/c8d557/kube-apiserver-proxy:amd64@sha256:
+    -t 248837585826.dkr.ecr.ap-northeast-1.amazonaws.com/c8d557/kube-apiserver-proxy
+    */
   }
-};
+}
 
 class DeploymentStack extends cdktf.TerraformStack {
   constructor(scope: Construct, id: string, props: { metadataFilePath: string; }) {
     super(scope, id);
-    new LocalProvider(this, "LocalProvider");
-    new AwsProvider(this, "Aws", { region });
-    const adapter = new AwsTerraformAdapter(this, "AwsAdapter");
+    new LocalProvider(this, 'LocalProvider');
+    new AwsProvider(this, 'Aws', { region });
+    const adapter = new AwsTerraformAdapter(this, 'AwsAdapter');
     const host = this.node.findChild('AwsAdapter');
 
     const file = new DataLocalFile(this, 'metadata-json', {
@@ -152,58 +196,79 @@ class DeploymentStack extends cdktf.TerraformStack {
     });
 
     const user = new User(adapter, 'K3sUser');
-    const imageName = cdktf.Fn.lookupNested(cdktf.Fn.jsondecode(file.content), ["iam-pgbouncer", "image.name"]);
-    const repositoryName = cdktf.Fn.trimprefix(imageName, `${cdktf.Fn.element(cdktf.Fn.split("/", imageName), 0)}/`);
+    adapter.exportValue(user.userName, {
+      name: 'K3sUserNameOutput',
+    });
+
+    const imageName: string = cdktf.Fn.lookupNested(cdktf.Fn.jsondecode(file.content), ['iam-pgbouncer-arm64', 'image.name']);
+    let repositoryName = cdktf.Fn.trimprefix(
+      imageName,
+      cdktf.Fn.element(cdktf.Fn.split('/', imageName), 0) as string + '/',
+    );
+    repositoryName = cdktf.Fn.element(cdktf.Fn.split(':', repositoryName), 0);
     const repository = Repository.fromRepositoryName(adapter, 'Repository', repositoryName);
     const database = DatabaseInstance.fromDatabaseInstanceAttributes(adapter, 'Database', {
       instanceEndpointAddress: 'default-postgres.cwggjv4mxugb.us-west-2.rds.amazonaws.com',
       port: 5432,
-      instanceResourceId: 'foo',
+      instanceResourceId: 'db-R4XUY7T35NHLEA3FNCS6AZGJYQ',
       instanceIdentifier: 'default-postgres',
       securityGroups: [],
     });
-    repository.grantPull(user);
-    database.grantConnect(user, 'k3s');
 
-    adapter.exportValue(user.userName, {
-      name: 'K3sUserNameOutput'
-    });
+    repository.grantPull(user);
+    const grant = database.grantConnect(user, 'k3s');
+    grant.principalStatements[0].addResources(
+      cdk.Arn.format({
+        ...cdk.Arn.split(grant.principalStatements[0].resources[0], cdk.ArnFormat.COLON_RESOURCE_NAME),
+        region: 'us-west-2',
+      })
+    );
 
     const buckets = [];
     cdktf.Aspects.of(cdktf.TerraformStack.of(this)).add({
-      visit: (node) => {
+      visit: node => {
         buckets.push(node);
       },
     });
 
     const accessKey = new IamAccessKey(this, 'K3sAccessKey', {
       user: cdktf.Lazy.stringValue({
-        produce: ({ scope }) => {
+        produce: ({ scope: stack }) => {
           try {
-            invokeAspects(scope);
-            cdktf.Aspects.of(scope)._aspects = [];
+            invokeAspects(stack);
+            // @ts-expect-error Private property
+            // eslint-disable-next-line no-underscore-dangle
+            cdktf.Aspects.of(stack)._aspects = [];
           } catch (error) {
             console.warn(error);
           }
           return host.resolveAtt(adapter.resolve(user.userName).Ref, 'UserName');
-        }
+        },
       }),
+    });
+
+    new cdktf.TerraformOutput(this, 'K3sAccessKeyIdOutput', {
+      value: accessKey.id,
+    });
+    new cdktf.TerraformOutput(this, 'K3sSecretAccessKeyOutput', {
+      value: accessKey.secret,
+      sensitive: true,
     });
   }
 }
 
 const app = new cdktf.App();
-const imagesStack = new ImagesStack(app, "ImagesStack");
+const imagesStack = new ImagesStack(app, 'ImagesStack');
 const { metadataFilePath } = imagesStack;
-const stack = new DeploymentStack(app, "Stack", {
+const deploymentStack = new DeploymentStack(app, 'DeploymentStack', {
   metadataFilePath,
 });
-stack.addDependency(imagesStack);
+deploymentStack.addDependency(imagesStack);
 
-new cdktf.CloudBackend(stack, {
-  hostname: "app.terraform.io",
-  organization: "sehyun-hwang",
-  workspaces: new cdktf.NamedCloudWorkspace("k3s-playground")
+new cdktf.CloudBackend(deploymentStack, {
+  hostname: 'app.terraform.io',
+  organization: 'sehyun-hwang',
+  workspaces: new cdktf.NamedCloudWorkspace('k3s-playground'),
 });
 
 app.synth();
